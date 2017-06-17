9596ff6106cd39ceb8267582433583f98950d074
Olivier de WITTE <olivier.dewitte@ext.taktik.com>
2017-06-17 20:21:45 +0200

    fix code

--- main.py
+++ main.py
@@ -3,13 +3,15 @@ import json
 
 url = 'https://api.irail.be/liveboard/?station=Yvoir&fast=true&format=json'
 
-req = urllib.Request(url)
+req = urllib2.Request(url)
 handle = urllib2.urlopen(req)
 
 res = handle.read()
 res = json.loads(res)
 
-print "Station - Retard - Quai"
+data = []
 for depart in res.get('departures').get('departure'):
-        print "{0} {1} {2}".format(depart.get('station'), depart.get('delay'), depart.get('platform'))
-        
+        # print "{0} {1}".format(depart.get('station'), depart.get('delay'), depart.get('platform'))    
+        data.append({'departures': depart.get('station'), 'delay': depart.get('delay'), 'platform': depart.get('platform')})
+
+print json.dumps(data)
