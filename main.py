adc2e4b5701db5308c3c5626d80cb4d91468e433
Olivier de WITTE <olivier.dewitte@ext.taktik.com>
2017-06-17 20:33:01 +0200

    add comment

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
+# -*- coding: utf-8 -*-
 import urllib2
 import json
 
 url = 'https://api.irail.be/liveboard/?station=Yvoir&fast=true&format=json'
 
+# Récupération des données via urllib2
 req = urllib2.Request(url)
 handle = urllib2.urlopen(req)
-
 res = handle.read()
+
+# Transformation des données au format json en un dictionaire python
 res = json.loads(res)
 
+# Boucle sur les départs
 data = []
 for depart in res.get('departures').get('departure'):
-        # print "{0} {1}".format(depart.get('station'), depart.get('delay'), depart.get('platform'))    
         data.append({'departures': depart.get('station'), 'delay': depart.get('delay'), 'platform': depart.get('platform')})
 
 print json.dumps(data)
